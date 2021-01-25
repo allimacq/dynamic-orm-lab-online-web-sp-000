d8780106e46eb3c12bbb92a892c4875e50a3fba1
Allison _ <allimacq@gmail.com>
2021-01-25 17:20:48 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,6 +56,6 @@ class InteractiveRecord
   
   def self.find_by(attributes = {})
     p attributes.keys
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attributes} = ?"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attributes.keys} = #{attributes.values}"
   end
 end
